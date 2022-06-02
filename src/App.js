f5c44cb62e4ad31395ce1008a745c75cb809a866
Nathan <nathancread@gmail.com>
2022-06-02 01:31:11 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import AwesomeSlider from "react-awesome-slider";
+import "react-awesome-slider/dist/styles.css";
 
-function App() {
+export default function App() {
+  const data = [
+    {
+      id: "1",
+      icon: "asset/mobile.png",
+      title: "Web Design",
+      desc:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+      img:
+        "https://i.imgur.com/JNGZyXf.jpg"
+    },
+    {
+      id: "2",
+      icon: "asset/globe.png",
+      title: "Mobile Application",
+      desc:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      img:
+        "https://i.imgur.com/OTaXMbg.jpg"
+    },
+    {
+      id: "3",
+      icon: "asset/writing.png",
+      title: "Branding",
+      desc:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      img:
+        "https://i.imgur.com/w9saCm7.jpg"
+    }
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AwesomeSlider>
+        {data.map((d) => (
+          <div className="item">
+            <div className="left">
+              <div className="leftContainer">
+                <div className="imgContainer">
+                  <img src={d.img} />
+                </div>
+                <h2>{d.title}</h2>
+                <p>{d.desc} </p>
+              </div>
+            </div>
+
+            
+          </div>
+        ))}
+      </AwesomeSlider>
     </div>
   );
 }
-
-export default App;
