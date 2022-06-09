5a13d78650f00de72c3db99cb729443a56b1884b
Nathan <nathancread@gmail.com>
2022-06-08 19:07:56 -0500

    sliders

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import AwesomeSlider from "react-awesome-slider";
-import "react-awesome-slider/dist/styles.css";
-
+import AwesomeSliderStyles from 'react-awesome-slider/src/styles';
+// import "react-awesome-slider/dist/styles.css";
+import "./fixSlider.css"
+import * as sliderSettings from "./sliderSettings.js"
 export default function App() {
   const data = [
     {
@@ -32,24 +34,46 @@ export default function App() {
     }
   ];
   return (
-    <div className="App">
-      <AwesomeSlider>
-        {data.map((d) => (
+    // <div className="App">
+    //   <AwesomeSlider cssModule={AwesomeSliderStyles}>
+    //     {data.map((d) => (
+    //       <div className="item">
+    //         <div className="left">
+    //           <div className="leftContainer">
+    //             <div className="Background-component">
+    //               <img src={d.img} />
+    //             </div>
+    //             <h2>{d.title}</h2>
+    //             <p>{d.desc} </p>
+    //           </div>
+    //         </div>
+
+            
+    //       </div>
+    //     ))}
+    //   </AwesomeSlider>
+    // </div>
+
+<AwesomeSlider style={ sliderSettings.sliderSettings}
+                                    fillParent={true}>
+                                       {data.map((d) => (
           <div className="item">
             <div className="left">
               <div className="leftContainer">
-                <div className="imgContainer">
+                <div className="Background-component">
                   <img src={d.img} />
                 </div>
                 <h2>{d.title}</h2>
                 <p>{d.desc} </p>
               </div>
             </div>
-
-            
-          </div>
-        ))}
-      </AwesomeSlider>
-    </div>
+           </div>
+           ))}
+          </AwesomeSlider>
+                              //   media={
+                              //     [{ "source": "https://i.imgur.com/JNGZyXf.jpg" },
+                              //     { "source": "https://i.imgur.com/OTaXMbg.jpg" },
+                              //     { "source": "https://i.imgur.com/w9saCm7.jpg" }]
+                              // }
   );
 }
---
+++ src/sliderSettings.js
@@ -0,0 +1,14 @@
+export const sliderSettings = { 
+"--slider-height-percentage": "64%",
+"--slider-transition-duration": "738ms",
+"--organic-arrow-thickness": "13px",
+"--organic-arrow-border-radius": "30px",
+"--organic-arrow-height": "50px", "--organic-arrow-color": "#d5cfb5",
+"--control-button-width": "10%", "--control-button-height": "25%",
+"--control-button-background": "transparent", "--control-bullet-color": "#4a9c8c",
+"--control-bullet-active-color": "#3f8577", "--loader-bar-color": "#851515",
+"--loader-bar-height": "16px" 
+}
+
+
+
