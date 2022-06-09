b56cf9303b8882103a233ba5bc7cd0a54ba85bd8
Nathan <nathancread@gmail.com>
2022-06-09 13:02:28 -0500

    some styling

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ export default function App() {
     {
       id: "1",
       icon: "asset/mobile.png",
-      title: "Web Design",
+      title: "Byron",
       desc:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        "The freezing wind bites at your face as you survey the barren tundra. Stinking corpses piled dozens high, charred beyond recognition to prevent their reawakening. ",
       img:
         "https://i.imgur.com/Km4RKjj.jpg"
     },
@@ -41,6 +41,7 @@ export default function App() {
     left: 0,
     top: 0,
     width: "100%"
+
   };
 
   return (
@@ -64,27 +65,28 @@ export default function App() {
     //   </AwesomeSlider>
     // </div>
 
-    <AwesomeSlider style={sliderSettings.sliderSettings}
+    <AwesomeSlider className="App" style={sliderSettings.sliderSettings}
       fillParent={true}>
       {data.map((d) => (
         // <div className="item">
         //   <div className="left">
         //     <div className="leftContainer">
-        <div style={{ zIndex: 2 }}>
+        <div style={{ zIndex: 2 }} >
+
+          <div className="Byron">
+            <div className="Byron-title-container">
+              <div className="Byron-title">{d.title}</div>
+            </div>
+            <div className="Byron-description-container">
+              <div className="Byron-description">{d.desc}</div>
+            </div>
+          </div>
           <div className='page'>
             <img style={bgImg} src={d.img} />
           </div>
-          <div className="Byron">
-                <h1 className="Byron-title">BYRON</h1>
-                <p>The freezing wind bites at your face as you survey the barren tundra in front of you. Stinking corpses piled dozens high, charred beyond recognition to prevent their reawakening.  </p>
-          </div>
-
         </div>
-        //       <h2>{d.title}</h2>
-        //       <p>{d.desc} </p>
-        //     </div>
-        //   </div>
-        //  </div>
+
+
       ))}
     </AwesomeSlider>
     //   media={
