511a693b1a5a7da07237571ed9a2aaf837fdc31f
Nathan <nathancread@gmail.com>
2022-07-18 16:03:33 -0500

    some styling done

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import AwesomeSlider from "react-awesome-slider";
 import AwesomeSliderStyles from 'react-awesome-slider/src/styles';
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
+import ReactCardFlip from 'react-card-flip';
 
 import * as sliderSettings from "./sliderSettings.js"
 import "./App.css"
+import "./Byron.css"
 export default function App() {
+  
   const data = [
     {
       id: "1",
@@ -14,7 +17,7 @@ export default function App() {
       desc:
         "The freezing wind bites at your face as you survey the barren tundra. Stinking corpses piled dozens high, charred beyond recognition to prevent their reawakening. ",
       img:
-        "https://i.imgur.com/Km4RKjj.jpg"
+        "https://i.imgur.com/w6qSGWC.jpg"
     },
     {
       id: "2",
@@ -37,11 +40,11 @@ export default function App() {
   ];
   const handleDragStart = (e) => e.preventDefault();
 
-  const items = [
-    <img className="Slider-style" src="https://i.imgur.com/7roU439.png" onDragStart={handleDragStart} role="presentation" />,
-    <img className="Slider-style"  src="https://imgur.com/YRJLDPN.jpg" onDragStart={handleDragStart} role="presentation" />,
-    <img className="Slider-style"  src="https://i.imgur.com/w9saCm7.jpg" onDragStart={handleDragStart} role="presentation" />,
-  ];
+  const backgrounds = [
+    <img key="background1" className="Slider-style" src="https://i.imgur.com/7roU439.png" onDragStart={handleDragStart} role="presentation" />,
+    <img key="background2" className="Slider-style" src="https://imgur.com/YRJLDPN.jpg" onDragStart={handleDragStart} role="presentation" />,
+    <img key="background3" className="Slider-style" src="https://i.imgur.com/w9saCm7.jpg" onDragStart={handleDragStart} role="presentation" />,
+  ]
   const bgImg = {
     position: "absolute",
     zIndex: -1,
@@ -54,37 +57,38 @@ export default function App() {
   return (
 
 
-    <AwesomeSlider className="App" style={sliderSettings.sliderSettings}
-      fillParent={true}>
+    <AwesomeSlider className="App" style={sliderSettings.sliderSettings} fillParent={true}>
       {data.map((d) => (
-        <div style={{ zIndex: 2 }} >
-          <div className="Slider-frame">
-            <AliceCarousel mouseTracking  items={items} disableButtonsControls={false} infinite={true}   />
-          </div>
-          <div className="Slider-frame-2">
-            <AliceCarousel  mouseTracking items={items} disableButtonsControls={false} infinite={true}   />
-          </div>
-          <div className="Byron">
-            <div className="Byron-title-container">
-              <div className="Byron-title">{d.title}</div>
-            </div>
-            <div className="Byron-description-container">
-              <div className="Byron-description">{d.desc}</div>
-            </div>
-          </div>
-          <div className='page'>
+        // <div style={{ zIndex: 2 }} >
+        //   <div className="Slider-frame">
+        //     <AliceCarousel mouseTracking  backgrounds={backgrounds} disableButtonsControls={false} infinite={true}   />
+        //   </div>
+        //   <div className="Slider-frame-2">
+        //     <AliceCarousel  mouseTracking backgrounds={backgrounds} disableButtonsControls={false} infinite={true}   />
+        //   </div>
+        //   <div className="Byron">
+        //     <div className="Byron-title-container">
+        //       <div className="Byron-title">{d.title}</div>
+        //     </div>
+        //     <div className="Byron-description-container">
+        //       <div className="Byron-description">{d.desc}</div>
+        //     </div>
+        //   </div>
+        <div className="App" >
+          <div key={d.key} className='page'>
             <img style={bgImg} src={d.img} />
+            <div className="Page-left">
+              <div className="Byron-left"></div>
+            </div>
+            <div className="Page-center"></div>
+            <div className="Page-right"></div>
           </div>
         </div>
 
 
       ))}
     </AwesomeSlider>
-    //   media={
-    //     [{ "source": "https://i.imgur.com/JNGZyXf.jpg" },
-    //     { "source": "https://i.imgur.com/OTaXMbg.jpg" },
-    //     { "source": "https://i.imgur.com/w9saCm7.jpg" }]
-    // }
+
 
   );
 }
--- src/sliderSettings.js
+++ src/sliderSettings.js
@@ -4,7 +4,7 @@ export const sliderSettings = {
 "--organic-arrow-thickness": "13px",
 "--organic-arrow-border-radius": "20px",
 "--organic-arrow-height": "50px", "--organic-arrow-color": "#d5cfb5",
-"--control-button-width": "10%", "--control-button-height": "25%",
+"--control-button-width": "3%", "--control-button-height": "25%",
 "--control-button-background": "transparent", "--control-bullet-color": "#4a9c8c",
 "--control-bullet-active-color": "#3f8577", "--loader-bar-color": "#851515",
 "--loader-bar-height": "16px" 
